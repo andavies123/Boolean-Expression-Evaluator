d687d86e6f2a21965c474f376990ceb671c62f20
Andavies123 <davies.andrew.123@gmail.com>
2019-09-19 14:46:12 -0700

    Update boolean_expression.js
    - Now the infix expression gets converted to postfix expression.
    - Only works with & and |.

--- boolean_expression.js
+++ boolean_expression.js
@@ -2,9 +2,71 @@ const expressionInput = document.querySelector('#expression_input');
 const calculateBtn = document.querySelector('#calculate_button');
 
 calculateBtn.addEventListener('click', e=> {
-    parseFunction(expressionInput.value);
+    parseFunction(expressionInput.value.replace(/\s/g, ""));
 })
 
+var stack;
+var identifiers;
+var postfixExpression;
+
+var precAND = 1;
+var precOR = 2;
+
 function parseFunction(expression) {
-    
+    stack = [];
+    identifiers = [];
+    postfixExpression = "";
+    convertToPostfix(expression);
+    console.log('stack: ' + stack);
+    console.log('identifiers: ' + identifiers);
+    console.log('newExpression: ' + postfixExpression);
+}
+
+function convertToPostfix(expression) {
+    for(i = 0; i < expression.length; i++) {
+        evaluateToken(expression.charAt(i));
+    }
+    while(stack.length !== 0) {
+        postfixExpression += stack.pop();
+    }
+}
+
+function evaluateToken(token) {
+    if(token === '(')
+        stack.push(token);
+    else if(token === ')')
+        stack.push(token);
+    else if(token === '&' || token === '|')
+        evaluateOperand(token);
+    else if(token === '!')
+        stack.push(token);
+    else if(isLetter(token) && !identifiers.includes(token)) {
+        identifiers.push(token);
+        postfixExpression += token;
+    }
+    else if(isLetter(token) && identifiers.includes(token)) {
+        postfixExpression += token;
+    }
+}
+
+function evaluateOperand(operand) {
+    var operandPrec = getOperandPrecedence(operand);
+    if(stack.length === 0 || operandPrec < getOperandPrecedence(stack[stack.length - 1])) {
+        stack.push(operand);
+    }
+    else {
+        postfixExpression += stack.pop();
+        evaluateOperand(operand);
+    }
+}
+
+function getOperandPrecedence(operand) {
+    if(operand == '&')
+        return precAND;
+    else if(operand == '|')
+        return precOR;
+}
+
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
 }
