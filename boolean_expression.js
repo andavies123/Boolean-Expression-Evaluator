e10083f66e2c9a081a3db9bd2ce3e1374c522cbd
Andavies123 <davies.andrew.123@gmail.com>
2019-09-19 02:00:07 -0700

    Create boolean_expression.js
    - Added javascript file to parse the given expression

---
+++ boolean_expression.js
@@ -0,0 +1,10 @@
+const expressionInput = document.querySelector('#expression_input');
+const calculateBtn = document.querySelector('#calculate_button');
+
+calculateBtn.addEventListener('click', e=> {
+    parseFunction(expressionInput.value);
+})
+
+function parseFunction(expression) {
+    
+}
